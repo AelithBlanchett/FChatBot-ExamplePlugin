e4f74b646b520b840abd5b8c1f45a31eeac31585
Aelith Blanchett <tonyteur@yahoo.fr>
2016-06-29 11:58:59 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+//adapted from https://github.com/AelithBlanchett/fchatlib/blob/master/test/app.js
+var FChatLib = require('fchatlib');
+
+////with config file
+//var options = require(__dirname+'/config/config.js');
+////without config file
+var options = {username: "MyAccount", password: "MyPassword", character: "MyNewShinyRobot", room: "ADH-someid"}; //you can obtain the room's ID you want the bot in by typing /code in F-chat
+
+var myFchatBot = new FChatLib(options);
+console.log("ok");
