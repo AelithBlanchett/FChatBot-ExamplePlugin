8315394908eeff4ec14a9d262964d345507d4119
Aelith Blanchett <tonyteur@yahoo.fr>
2017-08-31 23:59:21 -0400

    Updated plugin to be compatible with FChatLib ^1.0.0

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 //adapted from https://github.com/AelithBlanchett/fchatlib/blob/master/test/app.js
-var FChatLib = require('fchatlib');
+var FChatLib = require("fchatlib").default;
 
-////with config file
-//var options = require(__dirname+'/config/config.js');
-////without config file
-var options = {username: "MyAccount", password: "MyPassword", character: "MyNewShinyRobot", master: "YourCharacter", room: "ADH-someid"}; //you can obtain the room's ID you want the bot in by typing /code in F-chat
-
+var options = {username: "MyAccount", password: "MyPassword", character: "MyNewShinyRobot", room: "ADH-someid"}; //you can obtain the room's ID you want the bot in by typing /code in F-chat
 var myFchatBot = new FChatLib(options);
-console.log("ok");
+myFchatBot.connect();
--- plugins/my_plugin.js
+++ plugins/my_plugin.js
@@ -1,46 +1,44 @@
-var fChatLibInstance;
-var channel;
+var CommandHandler = function(fChatClient, channel){
 
-module.exports = function (parent, chanName) {
-    fChatLibInstance = parent;
+    this.channel = channel;
+    this.fChatClient = fChatClient;
 
-    var cmdHandler = {};
-    channel = chanName;
-
-    cmdHandler.guide = function (args, data) {
+    //Commands
+    this.guide = function (args, data) {
         console.log(data);
         var message = "Here's a few things to start: \n";
         message += "The command received the following args: "+JSON.stringify(args)+"\n";
         message += "The command received the following data: "+JSON.stringify(data)+"\n";
         message += "The character who sent the message is:"+data.character+"\n";
-        message += "The message was received in the channel:"+data.channel+ " and it's also passed everytime here: "+channel+"\n";
+        message += "The message was received in the channel:"+data.channel+ " and it's also passed everytime here: "+this.channel+"\n";
         message += "You can call functions outside of the scope: "+getRandomInt(0,10)+"\n";
         message += "And you'll also be able to do things from there, like rolling a die. "+rollDie()+"\n";
-        message += "The users in the room are: "+fChatLibInstance.getUserList(channel)+"\n";
-        message += "Here's the list of all the users, in all the room the bot is connected to: "+JSON.stringify(fChatLibInstance.getAllUsersList())+"\n";
-        message += "The operators in this room are: "+JSON.stringify(fChatLibInstance.getChatOPList(channel))+ "\n";
+        message += "The users in the room are: "+this.fChatClient.getUserList(this.channel)+"\n";
+        message += "Here's the list of all the users, in all the room the bot is connected to: "+JSON.stringify(this.fChatClient.getAllUsersList())+"\n";
+        message += "The operators in this room are: "+JSON.stringify(this.fChatClient.getChatOPList(this.channel))+ "\n";
         message += "As you will see in a few seconds, the results of the die roll will be shown. There are many actions that you can listen on by passing a function to them, here's the list:\n";
         message += "addMessageListener, addOfflineListener, addLeaveListener, addJoinListener, addChatOPAddedListener, addChatOPRemovedListener, addInitialChannelDataListener\n";
         message += "Alright, you must be ready now. You don't have to re-run the bot each time you update the plugin, just do !reloadplugins in the chat!";
-        fChatLibInstance.sendMessage(message, channel);
+        this.fChatClient.sendMessage(message, this.channel);
     };
 
-    fChatLibInstance.addRollListener(function(parent, data){
-        fChatLibInstance.sendMessage("Oh! Someone rolled a die! They rolled a "+data.endresult+" but here's the data: "+JSON.stringify(data), channel);
+    //Listener for events
+    this.fChatClient.addRollListener(function(data){
+        this.sendMessage("Oh! Someone rolled a die! They rolled a "+data.endresult+" but here's the data: "+JSON.stringify(data), data.channel);
     });
 
-    fChatLibInstance.addJoinListener(function(parent, data){
-        fChatLibInstance.sendMessage("Yay, someone joined the channel! Welcome "+data.character.identity, channel);
+    this.fChatClient.addJoinListener(function(data){
+        this.sendMessage("Yay, someone joined the channel! Welcome "+data.character.identity, data.channel);
     });
-
-    return cmdHandler;
 };
 
-function getRandomInt(min, max) {
+var getRandomInt = function(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
-}
+};
 
-function rollDie() {
-    fChatLibInstance.roll("1d6", channel);
+var rollDie = function() {
+    this.fChatClient.roll("1d6", channel);
     return "I rolled a die, and since I added a listener with the addRollListener method, I'll be able to know who rolled, and what's the result!";
-}
+};
+
+module.exports.CommandHandler = CommandHandler;
